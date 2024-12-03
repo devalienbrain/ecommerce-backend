a5076d5015ed563d580fd2be729a53b3255b49de
sabbirhassanict <sabbirmurad.doict@gmail.com>
2024-12-04 02:08:20 +0600

    Login successfull and token generated: OK

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,6 +16,7 @@ export const register = async (req, res) => {
     const user = await prisma.user.create({
       data: { email, password: hashedPassword, role },
     });
+    console.log("Hashed Password (Register):", hashedPassword);
 
     res.status(201).json({ message: "User registered successfully", user });
   } catch (err) {
@@ -25,20 +26,26 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-
+  console.log(email, password);
   try {
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) return res.status(404).json({ message: "User not found." });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials." });
+    console.log(user);
 
+    const isMatch = await bcrypt.compare(password.trim(), user.password.trim());
+    if (!isMatch) {
+      console.log("Password does not match");
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+    console.log("Password matches:", isMatch);
+    console.log(process.env.JWT_SECRET);
     const token = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
+    console.log({ token });
     res.status(200).json({ message: "Login successful", token });
   } catch (err) {
     res.status(400).json({ error: err.message });
